a15618cc3670c3cb3f26d7780e28d6cda9050d9a
kappa95 <federico.insero@mail.polimi.it>
2020-10-27 17:38:22 +0100

    preparing for reading the camera

--- src/second_trial.py
+++ src/second_trial.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 
+import rospy
+import yumi_utils as yumi
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge, CvBridgeError
+
+
