9a812cd7e7812b05e1a63fd5428795ca2f236d44
kappa95 <federico.insero@mail.polimi.it>
2020-11-01 18:11:14 +0100

    Added and tested the Orientation Constraint

--- src/demo_test.py
+++ src/demo_test.py
@@ -2,11 +2,12 @@
 
 import rospy
 import sys
+import copy
 from moveit_msgs.msg import *
 from moveit_commander import *
 import std_msgs.msg
 import geometry_msgs.msg
-from numpy import pi
+from yumi_utils import PI
 from yumi_hw.srv import *
 from yumi_utils import gripper_effort, reset_pose
 from tf.transformations import quaternion_from_euler
@@ -74,7 +75,7 @@ def run():
     group_r.set_goal_tolerance(0.005)
     group_r.set_num_planning_attempts(planning_attempts)
     group_r.set_planning_time(planning_time)
-
+    print(group_r.get_joints())
     # Both arms
     group_both = MoveGroupCommander("both_arms")
     # Type of planner
@@ -107,7 +108,7 @@ def run():
 
     # Defining the pose target object
     pose_target = geometry_msgs.msg.Pose()
-    p_target = [0.31500, -0.20200, 0.172, 0, pi, pi]
+    p_target = [0.31500, -0.20200, 0.172, 0, PI, PI]
     quaternion_target = quaternion_from_euler(p_target[3], p_target[4], p_target[5])
     pose_target.position.x = p_target[0]
     pose_target.position.y = p_target[1]
@@ -122,19 +123,45 @@ def run():
     plan = group_r.plan()
     group_r.execute(plan, wait=True)
     group_r.stop()
-    # Creating a Joint Constraint for the Right Arm
-    joint_constraint = JointConstraint()
-    # Imposing the last joint has 180 degrees of angle
-    joint_constraint.joint_name = group_r.get_joints()[-1]
-    joint_constraint.position = pi
-    joint_constraint.tolerance_above = 0.005
-    joint_constraint.tolerance_below = 0.005
-    joint_constraint.weight = 1.0
-    joint_constraint_list = [joint_constraint]
+
+    # # Creating a Joint Constraint for the Right Arm
+    # joint_constraint = JointConstraint()
+    # # Imposing the last joint has 180 degrees of angle
+    # joint_constraint.joint_name = group_r.get_joints()[-1]
+    # joint_constraint.position = -PI
+    # joint_constraint.tolerance_above = 0.005
+    # joint_constraint.tolerance_below = 0.005
+    # joint_constraint.weight = 1.0
+    # joint_constraint_list = [joint_constraint]
+    # constraint_list = Constraints()
+    # constraint_list.name = 'Picking and Placing joint orientation'
+    # constraint_list.joint_constraints = joint_constraint_list
+    # group_r.set_path_constraints(constraint_list)
+
+    # Setting the Orientation constraint
+    orientation_constraints = OrientationConstraint()
+    orientation_constraints.link_name = "gripper_r_base"
+    orientation_constraints.header.frame_id = "world"
+    orientation_constraints.orientation = copy.deepcopy(pose_target.orientation)
+    orientation_constraints.absolute_x_axis_tolerance = 0.1
+    orientation_constraints.absolute_y_axis_tolerance = 0.1
+    orientation_constraints.absolute_z_axis_tolerance = 0.1
+    orientation_constraints.weight = 1.0
+    # Constraints should be a list
+    orientation_constraint_list = [orientation_constraints]
+    # Declaring the object constraints
     constraint_list = Constraints()
-    constraint_list.name = 'Picking and Placing orientation'
-    constraint_list.joint_constraints = joint_constraint_list
+    constraint_list.orientation_constraints = orientation_constraint_list
     group_r.set_path_constraints(constraint_list)
+    pose_target.position.z = 0.180
+    group_r.set_start_state_to_current_state()
+    rospy.loginfo('Preparing new motion')
+    group_r.set_pose_target(pose_target)
+    rospy.sleep(1.0)
+    plan2 = group_r.plan()
+    print(plan2)
+    group_r.execute(plan2, wait=True)
+    group_r.clear_path_constraints()
 
 
 if __name__ == '__main__':
@@ -142,5 +169,7 @@ if __name__ == '__main__':
         # Initialization of the Node
         rospy.init_node('demo_test', anonymous=True)
         run()
+        rospy.loginfo('finished')
+        roscpp_shutdown()
     except Exception as e:
         print(e)
