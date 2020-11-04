18e26606290d22143cc1212149cff50e04186638
kappa95 <federico.insero@mail.polimi.it>
2020-11-04 19:00:04 +0100

    created a working scratch program for scanning barcodes

---
+++ src/test_barcode.py
@@ -0,0 +1,20 @@
+import zbar
+import numpy as np
+import cv2
+
+filename = 'image.bmp'
+# filename = 'provetta3.jpg'
+cv2.namedWindow('output', cv2.WINDOW_NORMAL)        # Create window with freedom of dimensions
+im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+imS = cv2.resize(im, (960, 540))
+cv2.imshow('output', im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+scanner = zbar.Scanner()
+results = scanner.scan(im)
+if not results:
+    print('oggetto nullo')
+else:
+    for result in results:
+        print(result.data)
