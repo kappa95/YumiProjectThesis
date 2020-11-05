db0e06b8c16a0db04300634827c526f6a14d71cd
kappa95 <federico.insero@mail.polimi.it>
2020-11-05 14:12:42 +0100

    improved the path of the filename

--- src/test_barcode.py
+++ src/test_barcode.py
@@ -1,7 +1,8 @@
+import os
 import zbar
 import cv2
 
-filename = 'image.bmp'
+filename = os.path.expanduser('~/image.bmp')
 # filename = 'provetta3.jpg'
 cv2.namedWindow('output', cv2.WINDOW_NORMAL)        # Create window with freedom of dimensions
 im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
@@ -9,11 +10,11 @@ imS = cv2.resize(im, (960, 540))
 cv2.imshow('output', im)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 scanner = zbar.Scanner()
 results = scanner.scan(im)
 if not results:
     print('oggetto nullo')
 else:
     for result in results:
-        print(result.data)
+        print(result)
+
