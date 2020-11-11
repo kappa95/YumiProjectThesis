4f541869f25b52ec6fad3f6723abc6d6c18dd764
kappa95 <federico.insero@mail.polimi.it>
2020-11-11 11:33:39 +0100

    starting demo

--- src/demo_test.py
+++ src/demo_test.py
@@ -6,9 +6,8 @@ from moveit_msgs.msg import *
 from moveit_commander import *
 from std_msgs.msg import String
 import geometry_msgs.msg
-from yumi_utils import PI
+from yumi_utils import PI, gripper_effort
 from yumi_hw.srv import *
-from yumi_utils import gripper_effort
 from tf.transformations import quaternion_from_euler, concatenate_matrices, euler_matrix, quaternion_from_matrix
 
 
@@ -43,7 +42,7 @@ rospy.init_node('demo_test', anonymous=True)
 rospy.loginfo('Starting the Initialization')
 roscpp_initialize(sys.argv)
 
-robot_yumi = RobotCommander()
+robot = RobotCommander()
 scene = PlanningSceneInterface()
 mpr = MotionPlanRequest()
 rospy.sleep(1.0)
@@ -74,7 +73,7 @@ group_r.set_workspace(ws=ws_R)
 
 # Replanning
 group_r.allow_replanning(True)
-group_r.set_goal_tolerance(0.0005)
+group_r.set_goal_tolerance(0.005)
 group_r.set_num_planning_attempts(planning_attempts)
 group_r.set_planning_time(planning_time)
 
@@ -94,42 +93,68 @@ group_both.set_planning_time(planning_time)
 # Publish the trajectory on Rviz
 rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
 rospy.sleep(1.0)
+
+# group_r.set_end_effector_link("gripper_r_base")
+end_effectorR = group_r.get_end_effector_link()
+rospy.loginfo('End effector R: {}'.format(end_effectorR))
+
+group_l.set_end_effector_link("gripper_l_base")
+end_effectorL = group_l.get_end_effector_link()
+rospy.loginfo('End effector L: {}'.format(end_effectorL))
+
+end_effectors = group_both.get_end_effector_link()
+rospy.loginfo('gli end effectors sono: {}'.format(end_effectors))
+
 rospy.loginfo('Finished init')
 
 # clean the scene
+
 rospy.loginfo('Cleaning of the objects in the scene')
-rospy.loginfo('Cleaning of the table')
-scene.remove_world_object("table")
+rospy.loginfo('Cleaning of the objects')
+try:
+    scene.remove_world_object("table")
+    scene.remove_world_object("input_rack")
+except Exception as e:
+    print(e)
 rospy.sleep(0.5)
 
 # add the table table
 rospy.loginfo('Adding the table object')
 table_pose = PoseStamped()
-table_pose.header.frame_id = robot_yumi.get_planning_frame()
+table_pose.header.frame_id = "yumi_body"
 table_pose.pose.position.x = 0.150 + table_width / 2
 table_pose.pose.position.y = 0.0
 table_pose.pose.position.z = table_height / 2
 scene.add_box("table", table_pose, size=(table_width, 1.2, table_height))
 
-home_L = [-0.43694, -1.92055, 0.91644, -1.39025, -1.47587, -0.00296, 0.74193]
-home_R = [0.60619, -2.07638, 0.88233, -1.65315, 1.69564, -0.21209, -0.35038]
+rospy.sleep(0.5)
+x_input_rack = 0.260
+y_input_rack = 0.175
+z_input_rack = 0.075
+input_rack_pose = PoseStamped()
+input_rack_pose.header.frame_id = "yumi_body"
+input_rack_pose.pose.position.x = 0.3465
+input_rack_pose.pose.position.y = 0.38090
+input_rack_pose.pose.position.z = table_height + 0.075/2
+scene.add_box("input_rack", input_rack_pose, size=(0.260, 0.175, 0.075))
+
+
+home_R = [1.6379728317260742, 0.20191457867622375, -2.5927578258514404, 0.538416862487793, 2.7445449829101562,
+          1.5043296813964844, 1.7523150444030762]
+home_L = [-1.46564781665802, 0.3302380442619324, 2.507143497467041, 0.7764986753463745, -2.852548837661743,
+          1.659092664718628, 1.378138542175293]
+
 home_joints = home_L + home_R
-# home_joints = [-1.407075546891484, -2.0968645586174586, 0.7081984499895384, 0.29709923791025084,
-#                -5.1407035742911944e-05, -7.242136084961714e-05, -5.345358943031897e-05,
-#                1.40707559518704, -2.096858749878596, -0.7081717891089125, 0.29710402150605564,
-#                -9.50240857688911e-05, -5.2985829942286955e-05, -3.199690151589607e-05]
 
 length_tube = 0.125  # [m]
 
 # Points useful: need to compute the pose
-# Rendezvous_picking_point
-rendezvous_picking_point = [0.31500, -0.20200, 0.172 + length_tube, 0, PI, PI]
+# Rendezvous_picking_point: center of the rack at an height of 10 cm more
 rendezvous_picking_pose = Pose()
-rendezvous_picking_pose.position.x = rendezvous_picking_point[0]
-rendezvous_picking_pose.position.y = rendezvous_picking_point[1]
-rendezvous_picking_pose.position.z = rendezvous_picking_point[2]
+rendezvous_picking_pose.position = copy.deepcopy(input_rack_pose.pose.position)
+rendezvous_picking_pose.position.z += 0.100  # [m]
 
-R1 = euler_matrix(rendezvous_picking_point[3], rendezvous_picking_point[4], rendezvous_picking_point[5])
+R1 = euler_matrix(0, PI, 0)
 R2 = euler_matrix(0, 0, PI / 4)
 quaternion_rendezvous_picking = quaternion_from_matrix(concatenate_matrices(R1, R2))
 rendezvous_picking_pose.orientation.x = quaternion_rendezvous_picking[0]
@@ -139,9 +164,10 @@ rendezvous_picking_pose.orientation.w = quaternion_rendezvous_picking[3]
 
 # Picking point
 pick = Pose()
-# TODO: Set the points
+# TODO: Set the points A1
 pick.position = copy.deepcopy(rendezvous_picking_pose.position)
-pick.position.z -= 0.050
+pick.position.x -= 0.115
+pick.position.y -= 0.073
 pick.orientation = copy.deepcopy(rendezvous_picking_pose.orientation)
 
 
@@ -150,21 +176,26 @@ def return_home():
     Return to the home position
     :return:
     """
-    state = robot_yumi.get_current_state()
+    state = robot.get_current_state()
     group_both.set_start_state(state)
     rospy.loginfo('Return to home for both arms')
     group_both.go(home_joints, wait=True)
+    group_both.stop()
 
 
 def picking():
-    state = robot_yumi.get_current_state()
+    state = robot.get_current_state()
     group_both.set_start_state(state)
-    group_r.set_pose_target(rendezvous_picking_pose)
-    group_r.go(wait=True)
+    group_l.set_pose_target(rendezvous_picking_pose)
+    group_l.go(wait=True)
+
+    # Open the gripper
+    gripper_effort(LEFT, -10)
+    gripper_effort(LEFT, 0)
     # Set the constraints for the picking:
     # Setting the Orientation constraint
     orientation_constraints = OrientationConstraint()
-    orientation_constraints.link_name = "gripper_r_base"
+    orientation_constraints.link_name = "gripper_l_base"
     orientation_constraints.header.frame_id = "yumi_body"
     orientation_constraints.orientation = copy.deepcopy(rendezvous_picking_pose.orientation)
     orientation_constraints.absolute_x_axis_tolerance = 0.1
@@ -176,92 +207,44 @@ def picking():
     # Declaring the object constraints
     constraint_list = Constraints()
     constraint_list.orientation_constraints = orientation_constraint_list
-    group_r.set_path_constraints(constraint_list)
-    group_r.set_pose_target(pick)
-    group_r.go(wait=True)
+    group_l.set_path_constraints(constraint_list)
+
+    # Go to pick position
+    group_l.set_pose_target(pick)
+    group_l.go(wait=True)
+    group_l.stop()
     rospy.sleep(1.0)
+    pick_up = group_l.get_current_pose()
+    pick.position.z = input_rack_pose.pose.position.z + z_input_rack/2 + 0.005
+    group_l.set_max_acceleration_scaling_factor(0.10)
+    group_l.set_pose_target(pick)
+    group_l.go(wait=True)
+    group_l.stop()
+    # picking
+    gripper_effort(RIGHT, 10)
+    # go up
+    group_l.set_pose_target(pick_up)
+    group_l.go(wait=True)
+    group_l.clear_path_constraints()
+    # returning to rendezvous
+    group_l.set_max_velocity_scaling_factor(1.0)
+    group_l.set_max_acceleration_scaling_factor(0.50)
+    group_l.set_pose_target(rendezvous_picking_pose)
+    group_l.go(wait=True)
+    group_l.stop()
 
 
 def run():
-    global robot_yumi
+    global robot
     global group_l
     global group_r
     global group_both
     global scene
     global mpr
 
-    return_home()
+    print('robot get planning frame {}'.format(robot.get_planning_frame()))
+
     picking()
-    # rospy.loginfo('Open the grippers')
-    # gripper_effort(LEFT, -20.0)
-    # # Relaxing gripper
-    # gripper_effort(LEFT, 0.0)
-    # gripper_effort(RIGHT, -20.0)
-    # # Relaxing gripper
-    # gripper_effort(RIGHT, 0.0)
-    #
-    # # Getting the current state of the robot: RobotState Message
-    # rospy.loginfo('getting the current state message')
-    # state = robot_yumi.get_current_state()
-    # group_both.set_start_state(state)
-    #
-    # # Defining the pose target object
-    # pose_target = Pose()
-    # p_target = [0.31500, -0.20200, 0.172, 0, PI, PI]
-    # quaternion_target = quaternion_from_euler(p_target[3], p_target[4], p_target[5])
-    # pose_target.position.x = p_target[0]
-    # pose_target.position.y = p_target[1]
-    # pose_target.position.z = p_target[2] + 0.300
-    # pose_target.orientation.x = quaternion_target[0]
-    # pose_target.orientation.y = quaternion_target[1]
-    # pose_target.orientation.z = quaternion_target[2]
-    # pose_target.orientation.w = quaternion_target[3]
-    # # Planning to the first position
-    # rospy.loginfo('Planning and Executing to the first position')
-    # group_r.set_pose_target(pose_target)
-    # plan = group_r.plan()
-    # group_r.execute(plan, wait=True)
-    # group_r.stop()
-
-    # # Creating a Joint Constraint for the Right Arm
-    # joint_constraint = JointConstraint()
-    # # Imposing the last joint has 180 degrees of angle
-    # joint_constraint.joint_name = group_r.get_joints()[-1]
-    # joint_constraint.position = -PI
-    # joint_constraint.tolerance_above = 0.005
-    # joint_constraint.tolerance_below = 0.005
-    # joint_constraint.weight = 1.0
-    # joint_constraint_list = [joint_constraint]
-    # constraint_list = Constraints()
-    # constraint_list.name = 'Picking and Placing joint orientation'
-    # constraint_list.joint_constraints = joint_constraint_list
-    # group_r.set_path_constraints(constraint_list)
-
-    # # Setting the Orientation constraint
-    # orientation_constraints = OrientationConstraint()
-    # orientation_constraints.link_name = "gripper_r_base"
-    # orientation_constraints.header.frame_id = "yumi_body"
-    # # orientation_constraints.header.frame_id = "world"
-    # orientation_constraints.orientation = copy.deepcopy(pose_target.orientation)
-    # orientation_constraints.absolute_x_axis_tolerance = 0.1
-    # orientation_constraints.absolute_y_axis_tolerance = 0.1
-    # orientation_constraints.absolute_z_axis_tolerance = 0.1
-    # orientation_constraints.weight = 1.0
-    # # Constraints should be a list
-    # orientation_constraint_list = [orientation_constraints]
-    # # Declaring the object constraints
-    # constraint_list = Constraints()
-    # constraint_list.orientation_constraints = orientation_constraint_list
-    # group_r.set_path_constraints(constraint_list)
-    # pose_target.position.z = 0.180
-    # group_r.set_start_state_to_current_state()
-    # rospy.loginfo('Preparing new motion')
-    # group_r.set_pose_target(pose_target)
-    # rospy.sleep(1.0)
-    # plan2 = group_r.plan()
-    # print(plan2)
-    # group_r.execute(plan2, wait=True)
-    # group_r.clear_path_constraints()
 
 
 if __name__ == '__main__':
