f8f699c3dc39fdc766caf667f175db344bb146ae
kappa95 <federico.insero@mail.polimi.it>
2020-11-16 14:11:53 +0100

    Tested the code and fixed the boolean logic

--- src/BarcodeManager.py
+++ src/BarcodeManager.py
@@ -10,32 +10,35 @@ class BarcodeManager:
     def __init__(self, ):
         """Constructor for BarcodeManager"""
         self.barcode = None
-        self.loop_rate = rospy.Rate(100)
+        self.loop_rate = rospy.Rate(5)
         self.pub = rospy.Publisher('/barcode/response', Bool, queue_size=1)
+        self.response = False
         self.counter = 0
-        rospy.Subscriber('Barcode', String, self.callback)
+        rospy.Subscriber('barcode/barcode', String, self.callback)
 
     def callback(self, msg):
         try:
-            self.barcode = msg
-            rospy.logdebug('I received: {}'.format(msg))
+            self.barcode = msg.data
+            rospy.logdebug('I received: {}'.format(msg.data))
         except rospy.ROSException as err:
             rospy.logerr(err)
 
     def start(self):
         rospy.logdebug('start receiving')
         while not rospy.is_shutdown():
-            if self.barcode is not None:
+            if self.barcode:
+                self.response = True
                 rospy.loginfo('I received the barcode: {}'.format(self.barcode))
                 # TODO: Add the part for saving a json file with barcodes position (use counter)
-                self.pub.publish(True)
+                self.pub.publish(self.response)
             else:
-                self.pub.publish(False)
+                self.response = False
+                self.pub.publish(self.response)
             self.loop_rate.sleep()
 
 
 if __name__ == '__main__':
     rospy.loginfo('BarcodeManager Started')
-    rospy.init_node('BarcodeManager')
+    rospy.init_node('BarcodeManager', log_level=rospy.DEBUG)
     node = BarcodeManager()
     node.start()
